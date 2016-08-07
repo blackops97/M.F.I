6ec9efb300b462882884c17c9e43184b21aebfdc
blackops97 <exetra29988@gmail.com>
2016-08-07 07:37:27 +0300

    Create en-getfile.lua

---
+++ plugins/en-getfile.lua
@@ -0,0 +1,27 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY SAJJAD HUSSIEN                 ▀▄ ▄▀ 
+▀▄ ▄▀   BY SAJJADHUSSIEN   (@sajjad_iq98)  ▀▄ ▄▀    
+▀▄ ▄  JUST WRITED BY SAJJAD HUSSIEN        ▀▄ ▄▀ 
+▀▄ ▄▀       get file  : جلب ملف            ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+local function run(msg, matches)
+  if matches[1] == "get file" then
+    local file = matches[2]
+    if is_sudo(msg) then --sudo only !
+      local receiver = get_receiver(msg)
+      send_document(receiver, "./plugins/"..file..".lua", ok_cb, false)
+      else 
+        return nil
+    end
+  end
+end
+
+return {
+  patterns = {
+  "^(get file) (.*)$"
+  },
+  run = run
+}
