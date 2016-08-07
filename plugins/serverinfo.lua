7dc961c0650362fc72caf8e45e9e1cc566cc7dc0
blackops97 <exetra29988@gmail.com>
2016-08-07 08:33:16 +0300

    Create serverinfo.lua

---
+++ plugins/serverinfo.lua
@@ -0,0 +1,21 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY SAJJAD HUSSIEN                 ▀▄ ▄▀ 
+▀▄ ▄▀   BY SAJJADHUSSIEN   (@sajjad_iq98)  ▀▄ ▄▀    
+▀▄ ▄  JUST WRITED BY SAJJAD HUSSIEN        ▀▄ ▄▀   
+▀▄ ▄▀   server info  :  معلومات السيرفر   ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+local function run(msg, matches)
+local text = io.popen("sh ./data/cmd.sh"):read('*all')
+if is_sudo(msg) then
+  return text
+end
+  end
+return {
+  patterns = {
+'^(معلومات السيرفر)$',},
+  run = run,
+  moderated = true
+}
