70280b9499af482343e4f9f7ad5c2eb5cd6c09c5
blackops97 <exetra29988@gmail.com>
2016-08-07 08:05:14 +0300

    Update en-voice.lua

--- plugins/en-voice.lua
+++ plugins/en-voice.lua
@@ -4,7 +4,7 @@
 ▀▄ ▄▀    BY SAJJAD HUSSIEN                 ▀▄ ▄▀ 
 ▀▄ ▄▀   BY SAJJADHUSSIEN   (@sajjad_iq98)  ▀▄ ▄▀    
 ▀▄ ▄  JUST WRITED BY SAJJAD HUSSIEN        ▀▄ ▄▀ 
-▀▄ ▄▀        voice  : صوت                  ▀▄ ▄▀ 
+▀▄ ▄▀        voice  : صوت                  ▀▄ ▄▀  
 ▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
 --]]
 do
