d721af977601c0481539d94046276c42a042ec89
blackops97 <exetra29988@gmail.com>
2016-08-08 23:31:45 +0300

    Update en-SAJAD_iq3.lua

--- plugins/en-SAJAD_iq3.lua
+++ plugins/en-SAJAD_iq3.lua
@@ -13,32 +13,25 @@ local function run(msg, matches)
 if is_sudo(msg) and matches[1]== "help dev" then
 return [[  It is shown by the channel orders @sajad_iq98 🌐
 
-✔️All orders to operate without setting / او !
+✔️All orders to operate without setting / - !
 〰️〰️〰️〰️〰️〰️〰️〰️〰️
 
-❇️ sosuper : Upgrade Super Group
+❇️ /addbot : To activate Group
 
-❇️ add : To activate Group
+❇️ /rembot : To disable Group
 
-❇️ rem : To disable Group
+❇️ /bot on : To run the bot to a certain group
 
-❇️ setowner : Raising Director of the total  
- 
-❇️ broadcast : Spread the word in all groups bot
+❇️ /bot off :  To extinguish the boot to a certain group
 
-❇️ bot on : To run the bot to a certain group
-
-❇️ bot off :  To extinguish the boot to a certain group
-
-❇️ addsudo : Add developer
-
-❇️ kickbot : To get out the bot of the group
+❇️ /leave : To get out the bot of the group
 
 ❇️ get file : Fetch the file from the server
 
 ❇️ isup : Server with a link to improve the bot stop
 
 ❇️ isup cron : Link with the server to improve server
+
 〰️〰️〰️〰️〰️〰️〰️〰️〰️
 ®️™️ dev 🔧 @sajjad_iq98
 ®️™️ channel 🔧 @sajad_iq98
